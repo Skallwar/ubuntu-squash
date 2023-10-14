1c838f891488ae7a0409ffe62b4cbf0855667eed
Antony Messerli <antony@mes.ser.li>
2023-10-13 23:45:19 -0500

    Add 23.10

--- version.sh
+++ version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
-VERSION=$(curl -sL http://cdimage.ubuntu.com/lubuntu/releases/lunar/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
+VERSION=$(curl -sL http://cdimage.ubuntu.com/lubuntu/releases/mantic/release/ |grep '<title>' | grep -Po "(\d+\.)+\d+")
 echo "${VERSION}"
